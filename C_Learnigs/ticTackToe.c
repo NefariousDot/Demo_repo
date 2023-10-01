14dbbbc2e571269c9b32931896e43e9cc70b99c1
NefariousDot <official.athmaram@gmail.com>
2023-10-01 23:13:20 +0530

    Simple TicTocktack game

--- C_Learnigs/ticTackToe.c
+++ C_Learnigs/ticTackToe.c
@@ -5,16 +5,15 @@
  ### Title : Tic Tack Toe Game
  ### Date : September 25 2023
  ####################################################################
- 
+
  */
 
 
 #include <stdio.h>
-//#include <conio.h>
 #include <stdlib.h>
 
 char cell[9] = {'1','2','3','4','5','6','7','8','9'};
-int player; 
+int player;
 
 int checkGameStatus()
 {
@@ -33,154 +32,151 @@ int checkGameStatus()
 
     /*
     ########################################
-    writing condition for win check 
+    writing condition for win check
     ########################################
     */
 
     //checking the row status
 
-    if( cell[0] == cell[1] == cell[2])
+    if( cell[0] == cell[1] && cell[1] == cell[2])
     gameStatus = 1;
 
-    else if( cell[3] == cell[4] == cell[5])
+    else if( cell[3] == cell[4] && cell[4] == cell[5])
     gameStatus = 1;
 
-    else if( cell[6] == cell[7] == cell[8])
+    else if( cell[6] == cell[7] && cell[7] == cell[8])
     gameStatus = 1;
 
     //checking the Column status
 
-    else if( cell[0] == cell[3] == cell[6])
+    else if( cell[0] == cell[3] && cell[3] == cell[6])
     gameStatus = 1;
 
-    else if( cell[1] == cell[4] == cell[7])
+    else if( cell[1] == cell[4] && cell[4] == cell[7])
     gameStatus = 1;
 
-    else if( cell[2] == cell[5] == cell[8])
+    else if( cell[2] == cell[5] && cell[5] == cell[8])
     gameStatus = 1;
 
     //checking the Diagonal status
 
-    else if( cell[2] == cell[4] == cell[6])
+    else if( cell[2] == cell[4] && cell[4] == cell[6])
     gameStatus = 1;
 
-    else if( cell[0] == cell[4] == cell[8])
+    else if( cell[0] == cell[4] && cell[4] == cell[8])
     gameStatus = 1;
 
     /*
     ########################################
-    writing condition for draw check 
+    writing condition for draw check
     ########################################
     */
 
    //if all the cells have different value than the original value we can consider it as a draw
 
-    else if( cell[0] != '1' && cell[1] != '2' && cell[2] != '3' && cell[3] != '4' && cell[4] != '5' 
+    else if( cell[0] != '1' && cell[1] != '2' && cell[2] != '3' && cell[3] != '4' && cell[4] != '5'
     && cell[5] != '6' && cell[6] != '7' && cell[7] != '8' && cell[8] != '9')
     gameStatus = 0;
 
     //if all the above conditions fails then game is still on
 
-    else 
+    else
     gameStatus = -1;
 
     return gameStatus;
-       
+
 }
 
 void displayGame()
 {
     //This is a simple function used to display the game.
 
-    system("cls");
+    system("clear");
+
+    printf("\n\nTIC TACK TOE GAME\n\n");
+    printf("Player 1 ==> 'X' \t\t Player 2 ==> 'O'\n\n");
+
+    printf("||          ||         ||          ||\n");
+    printf("||    %c     ||    %c    ||    %c     ||\n",cell[0],cell[1],cell[2]);
+    printf("||          ||         ||          ||\n");
+    printf("_____________________________________\n_____________________________________\n");
+    printf("||          ||         ||          ||\n");
+    printf("||    %c     ||    %c    ||    %c     ||\n",cell[3],cell[4],cell[5]);
+    printf("||          ||         ||          ||\n");
+    printf("_____________________________________\n_____________________________________\n");
+    printf("||          ||         ||          ||\n");
+    printf("||    %c     ||    %c    ||    %c     ||\n",cell[6],cell[7],cell[8]);
+    printf("||          ||         ||          ||\n");
     
-    printf("_____________________________________\n_____________________________________\n");
-    printf("||          ||         ||          ||\n");
-    printf("||    %c    ||    %c   ||    %c    ||\n",cell[0],cell[1],cell[2]);
-    printf("||          ||         ||          ||\n");
-    printf("_____________________________________\n_____________________________________\n");
-    printf("||          ||         ||          ||\n");
-    printf("||    %c    ||    %c   ||    %c    ||\n",cell[3],cell[4],cell[5]);
-    printf("||          ||         ||          ||\n");
-    printf("_____________________________________\n_____________________________________\n");
-    printf("||          ||         ||          ||\n");
-    printf("||    %c    ||    %c   ||    %c    ||\n",cell[6],cell[7],cell[8]);
-    printf("||          ||         ||          ||\n");
-    printf("_____________________________________\n_____________________________________\n");
-
-
 }
 
-int playerMarking()
+void playerMaking(int decision)
 {
     //This function is used to mark the changes made by players
 
-    int decisionValue;
-    player = 1;
-
-    displayGame();
-    
-    printf("\nPlayer %d make a decision : ", player);
-    scanf("%d", &decisionValue);
-
     //checking whether correct cell is selected or not
 
-    if(decisionValue > 9 || cell[decisionValue] == 'X' || cell[decisionValue] == 'O')
+    if(decision > 9 || cell[decision - 1] == 'X' || cell[decision - 1] == 'O')
     {
         printf("\nEntered a wrong value. Please re assign ");
         getchar();
+
+        player --;
     }
-
-
-    //Need to change the corresponding value in display
-
-    if(player == 1)
-    cell[decisionValue] = 'X';
-
-    else 
-    cell[decisionValue] = 'O';
-
-    player++;
-
-    return player;
+    else//Need to change the corresponding value in display
+    cell[decision - 1] = (player == 1) ? 'X' : 'O';
 
 }
 
 int main()
 {
-    int winner,status;
-
-    printf("TIC TACK TOE GAME\n\n");
-    printf("Player 1 ==> 'X' \t\t Player 2 ==> 'O'\n\n");
+    player = 1;
+    int decision, status;
+       
 
     do
     {
-        winner = playerMarking();
+        displayGame();
+
+        player = (player % 2) ? 1 : 2;
+
         status = checkGameStatus();
+        printf("status value is %d & player playing now is %d\n", status, player);
+
+        printf("\nPlayer %d make a decision : ", player);
+        scanf("%d", &decision);
+
+        playerMaking(decision);
+
+        status = checkGameStatus();
+        printf("status value is %d \n", status);
+        getchar();
+
+        player ++;
+
+        if (status == 1)
+        {
+            --player;
+            printf("\n****************************\n");
+            printf("    PLAYER %d WON      \n", player);
+            printf("****************************\n");
+        }
+        
+        else if (status == 0)
+        {
+            printf("\n****************************\n");
+            printf("         ITS A DRAW     \n");
+            printf("****************************\n");
+        }
+
+
     }while(status == -1);
-    
-
-    if (status == 1)
-    {
-        printf("\n****************************\n");
-        printf("    PLAYER %d WON      \n", player);
-        printf("****************************\n");
-
-    }
-
-    if (status == 0)
-    {
-        printf("\n****************************\n");
-        printf("    ITS A DRAW     \n");
-        printf("****************************\n");
-
-    }
 
 
     printf("\n\n****************************\n");
     printf("       GAME OVER      \n");
     printf("****************************\n");
 
-    return 0;  
+    return 0;
 
 }
